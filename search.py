fd93550e0dc25ebbb06708ba9edb813e0a30287d
Javier162380 <javierllorente16@gmail.com>
2017-12-21 23:12:12 +0100

    cambia la funcion este es el archivo que ejecutaremos

--- search.py
+++ search.py
@@ -8,9 +8,5 @@ api_token_secret=argv[4]
 
 twitter = twitter(api_key,api_secret,api_token,api_token_secret)
 #we are going to retrieve data about #ElClasico.
-twitter.streamingapi(['#ELclasico','#ElClasico','#ElClásico','#elclasico'], 'football','twitterjavierllorente'
-                     ,1,
-                     ['in_reply_to_status_id_str','favorite_count','in_reply_to_user_id_str','in_reply_to_status_id',
-                      'possibly_sensitive','in_reply_to_user_id','timestamp_ms',
-                      'is_quote_status','filter_level','favorited','quote_count','id_str','in_reply_to_screen_name',
-                      'source'])
+twitter.streamingapi(['Elclasico','elclásico','ElClasico','elclasico'], 'elclasico21102017'
+                     ,'twitterjavierllorente',0.8)
